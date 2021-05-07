fa8b890be807f11be3c29d4bf8e9010993a7d5fa
Carlos Maiello <carlosmaiello@gmail.com>
2021-05-07 19:54:55 -0300

    Revisão Comando for

---
+++ 0507/for.js
@@ -0,0 +1,17 @@
+
+console.log("DECREMENTANDO");
+
+for (var c = 10; c >= 1; c--) {
+    console.log(c);
+}
+
+console.log("DEPOIS", c);
+
+
+
+console.log("INCREMENTANDO");
+for (var c = 1; c <= 10; c++) {
+    console.log(c);
+}
+
+console.log("DEPOIS", c);
