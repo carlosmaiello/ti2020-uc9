722cbac847c0353acc73bda5e937e9a864f3533f
Carlos Maiello <carlosmaiello@gmail.com>
2021-05-07 22:23:47 -0300

    dowhile

--- 0507/dowhile.js
+++ 0507/dowhile.js
@@ -1,15 +1,15 @@
 const prompt = require('prompt-sync')();
 
-var sn = "s";
-var n1 = 0;
+var sn;
 
-while (sn == "s") {
-    n1 = +prompt("Digite um número: ");
+do {
+    var n1 = +prompt("Digite um número: ");
     console.log("Valor X 10 =", n1 * 10);
 
     do {
         sn = prompt("Digitar novamente (s/n)? ");
     } while (sn != "s" && sn != "n");
-}
+    
+} while (sn == "s");
 
 console.log("SAIU");
---
+++ 0507/testes.md
@@ -0,0 +1,16 @@
+```
+sn  n1      console
+s   0       Valor X 10 = 100
+    10      Valor X 10 = 110
+    11
+```
+
+```
+sn  n1      console
+s   0       Valor X 10 = 100
+n   10
+
+s   0       Valor X 10 = 100
+s   10      Valor X 10 = 110
+n   11
+```
